51bad19295d2158651da3b6ede0a706b7b77eff7
Erick-Llumiquinga <eam.llumiquinga@yavirac.edu.ec>
2019-12-28 23:04:57 -0500

    estructura

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors:{
+        purpura: "#716DF2",
+        azulOscuro: "#2D3040",
+        verdeClaro: "#57F29A",
+        rosado: "#F27272"
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+}
